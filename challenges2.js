6a240b1713e991e0c8fd642a220f2344f35c8acf
Ries <25RiesC@badinhs.org>
2025-05-01 10:51:07 -0400

    finished sum function

---
+++ challenges2.js
@@ -0,0 +1,47 @@
+//12 to 24 hr
+// while(){
+//      time=prompt( `"hh:mm:ss"`)
+//     char1=
+
+// }
+
+
+
+//smallest num divisable by 1-20
+// let count=0
+// let sum=1
+// while (count<20){//do the count function until it = 0
+//     count = count + 1
+//     console.log(count)
+
+// if(sum%count==0){//test if the remainder of dividing sum by count is zero
+//     console.log(sum)
+
+// }else{sum=sum+1}//if the remainder is not 0 add 1 to sum and test again
+// }
+
+
+//difference of squares
+let count=0
+let firstn=10
+let num1=0
+let num2=0
+let sum1=0
+let sum2=0
+while(count<firstn){
+    num2=count+1
+    count=count+1
+    num1=count**2
+    
+ 
+    sum1=sum1+num1
+    sum2=sum2+num2
+   
+}
+
+console.log(sum2**2 - sum1)
+
+
+// if(count<firstn){
+    
+// }else{console.log(`end`)}
